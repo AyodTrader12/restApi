7009d813de2a7415bbfad3ae32ae771cb1bb5f4b
Ayodtrader12 <ibrahimpopoola292@gmail.com>
2026-09-02 16:42:41 +0100

    server

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ let users = [
     id: 2,
     name: "Sarah",
     email: "sarah@gmail.com"
+  },
+  {
+    id:3,
+    name:"virtue",
+    email:"virtue@gmail.com"
+  },
+  {
+    id:4,
+    name:"gideon",
+    email:"gideon@gmail.com"
+  },
+  {
+    id:5,
+    name:"godswill",
+    email:"goddswill@gmail.com"
   }
 ];
 
@@ -18,7 +33,9 @@ const PORT = 3001;
 const app = express();
 
 app.use(express.json())
-
+app.get("/",(req,res) => {
+    res.send("welcome to my first express backend")
+})
 app.get("/users", (req, res) => {
   res.json(users);
 });
@@ -28,17 +45,17 @@ app.get("/users/:id",(req,res) => {
     try{
      const id = Number(req.params.id);
     const user = users.find(user =>user.id === id)
-    return res.status(201).json({
+    return res.status(200).json({
     message:"found user",
    data:user,
    status:200
 
-   
     })
+    
     }catch(err){
   if (!user) {
     return res.status(404).json({
-      message: err.message,
+      message:"User not found",
       status:404
     });
   }
@@ -58,26 +75,23 @@ const newUser = {
 users.push(newUser)
 console.log(users)
 return res.status(201).json({
-    message:"user created",
+    message:"user created successfully",
    data:newUser,
    status:201,
    
 });
 }catch(err){
     return res.status(404).json({
-        message: err.message,
+        message: "User not created",
         status:404
       });
 }
 })
 
 
-
-app.get("/users", (req, res) => {
-  res.json({
-    message: "Here are the users"
-  });
-});
+app.get("/users",(req,res) => {
+    res.json({message: "Here are the users", data:users})
+})
 
 
 
